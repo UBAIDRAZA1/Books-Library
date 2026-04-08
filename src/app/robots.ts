159886fa1008b3be5b718b2942d3929881e98092
UBAID RAZA <ubaidraza1@gmail.com>
2026-04-08 19:29:56 +0500

    Fix: Add static robots.txt file for Google crawler

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,1 @@
-import { MetadataRoute } from 'next'
-
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/api/', '/admin/'],
-    },
-    sitemap: 'https://books-library-ashy.vercel.app/sitemap.xml',
-  }
-}
+// Disabled - using public/robots.txt instead
